514dc3ab95fe5a1e201f0b55fbe76bc7dae83a7a
高境杉 <1359287622@qq.com>
2020-05-25 22:04:23 +0800

    seajs

--- 02_seajs/js/main.js
+++ 02_seajs/js/main.js
@@ -4,6 +4,17 @@
 }); */
 define(function (require, exports, module) {
   var circle = require('./circle.js');
+  var square = require('./square.js');
   alert(circle.mianji(10));
   alert(circle.zhouchang(10));
+
+  alert(square.mianji(10));
+  alert(square.zhouchang(10));
+
+  const kuang = document.getElementById('kuang');
+  const btn = document.getElementById('btn');
+  const h1 = document.getElementById('h1');
+  btn.onclick = function () {
+    h1.innerHTML = '圆的面积为' + circle.mianji(kuang.value);
+  };
 });
---
+++ 02_seajs/js/square.js
@@ -0,0 +1,4 @@
+define(function (require, exports, module) {
+  exports.mianji = (a) => a * a;
+  exports.zhouchang = (a) => 4 * a;
+});

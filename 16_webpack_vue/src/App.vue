57139db7b4be0925fffb522ea7322c39a9bdcc5f
高境杉 <1359287622@qq.com>
2020-06-04 20:33:40 +0800

    vue的组件

---
+++ 16_webpack_vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!-- changeMHan 后面不能加圆括号，加圆括号，就表示放弃那个参数了 -->
+        <Test :m = "m" :n = "n" @changeM="changeMHan" />
+        <Test :m = "m" :n = "n" @changeM="changeMHan" />
+    </div>
+</template>
+
+<script>
+    import Test from './components/Test.vue'
+    export default {
+        components: {
+            Test
+        },
+        data() {
+            return {
+                m: 5,
+                n:{
+                    v:4
+                }
+            }
+        },
+        methods:{
+            changeMHan({ bh }){
+                this.m += bh;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

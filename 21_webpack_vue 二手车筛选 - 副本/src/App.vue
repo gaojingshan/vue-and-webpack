f588af0a2a30187b5f8b4261fcc7ece4b37dfeee
高境杉 <1359287622@qq.com>
2020-06-07 11:34:37 +0800

    二手车筛选

--- 21_webpack_vue 二手车筛选 - 副本/src/App.vue
+++ 21_webpack_vue 二手车筛选 - 副本/src/App.vue
@@ -5,21 +5,19 @@
         <p class="pGrid">
             <!-- <input type="checkbox" :value="item" v-model="brand"> -->
             <RadioGroup v-model="brand">
-                <Radio :label="item" v-for="(item, index) in all.brand" :key="index">
+                <Radio :value="item" :label="item" v-for="(item, index) in all.brand" :key="index">
                     {{item}}
                 </Radio>
             </RadioGroup>
         </p>
         <!-- 颜色 -->
         <p class="pGrid">
-            <CheckboxGroup v-model="color">
-                <Checkbox v-for="(item, index) in all.color" :key="index" :label="item">
-                    {{item}}
-                </Checkbox>
-            </CheckboxGroup>
+            <label v-for="(item, index) in all.color" :key="index">
+                        <input type="checkbox" v-model="color" :value="item" >
+                        {{item}}
+                    </label>
         </p>
         <p>查询到<span>{{total}}</span>条相关车辆</p>
-        <!-- 表格 columns表示列 data表示数据 -->
         <Table border :columns="columns" :data="cardata"></Table>
         <p class="pageP">
             <Page :total="total" :current="current" :page-size="10" @on-change="changeHan" />
@@ -28,12 +26,9 @@
 </template>
 
 <script>
-    // 这个包专门用来拉Ajax的
     import axios from 'axios';
     export default {
-        // 生命周期，组件创建成功
         created() {
-            // 在生命周期中，调用loadData函数
             this.loadData();
             // 品牌
             axios.get('http://www.aiqianduan.com:56506/allbs').then(data => {
@@ -45,14 +40,12 @@
                         this.all.brand.push(pinpai)
                     }
                 }
-                // console.log(this.all.brand);
+                console.log(this.all.brand);
             })
         },
         data() {
             return {
-                 // 车辆总数
                 total: 0,
-                // 当前第几页
                 current: 1,
                 columns: [{
                         title: '图片',
@@ -100,41 +93,32 @@
                     }
                 ],
                 cardata: [],
-                // 罗列所有的筛选
                 all: {
                     color: ['红', '橙', '黄', '绿', '青', '蓝', '紫', '灰'],
                     brand: [],
                 },
                 // 选中的
-                color: [],
-                brand: ''
+                color: ['红', '黄'],
+                brand: '宝马'
             }
         },
         methods: {
             // 点击分页的时候
             changeHan(current) {
-                // 先改变current属性值
                 this.current = current;
-                // 然后调用拉取数据
                 this.loadData();
             },
-            // 提炼成函数
+            // 拉取Ajax封装为函数
             loadData() {
-                // 书写Ajax代码，拉取默认数据
                 axios.get('http://www.aiqianduan.com:56506/cars?page=' + this.current + '&color=' + this.color.join('v') + '&brand=' + this.brand).then(data => {
-                    // 回调函数，要把服务器返回的数据设置为data中的cardata
-                    // 两个data是固定写法，因为data中还封装了服务器的一些其他信息，data.data是返回的信息
                     this.cardata = data.data.results;
-                    // 把total替换掉
                     this.total = data.data.total;
                 })
             }
         },
-        // 监控
+        // 检测
         watch: {
-            // 监控color变化，函数名是什么，就表示监控什么
             color() {
-                // 当color变化了，就拉取数据,并且要变为第1页
                 this.current = 1;
                 this.loadData()
             },

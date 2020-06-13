10d8fe9e0d1d134eed1a65bf4bf0b72d8d34c8a8
高境杉 <1359287622@qq.com>
2020-06-13 19:23:32 +0800

    石头剪刀布

---
+++ 24_石头剪刀布/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button :class="{'cur' : chu == '石头'}" @click="chu = '石头'">石头</button>
+        <button :class="{'cur' : chu == '剪刀'}" @click="chu = '剪刀'">剪刀</button>
+        <button :class="{'cur' : chu == '布'}" @click="chu = '布'">布</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                chu: '石头'
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    .cur {
+        background: orange;
+    }
+</style>

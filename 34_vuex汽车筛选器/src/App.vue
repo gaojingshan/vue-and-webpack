fdf004b59ea2fc76d31ea1f53174a3ddcad6af8e
高境杉 <1359287622@qq.com>
2020-07-12 16:38:40 +0800

    vuex版汽车筛选器

--- 34_vuex汽车筛选器/src/App.vue
+++ 34_vuex汽车筛选器/src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="wrap">
+    <div style="padding: 10px 0">
+      <!-- 复选框不能使用v-model双向绑定了 -->
+      <!-- 因为双向绑定，就相当于直接更改全局数据了，就不能dispatch了。 -->
+      <!-- 这里的$event表示用户勾选之后的数组 -->
+      <CheckboxGroup :value="carStore.color" @on-change="$store.dispatch('carStore/changeColor',{color: $event})">
+        <Checkbox v-for="item in ['红','黄','蓝','黑','白']" :key="item" :label="item">
+          
+        </Checkbox>
+      </CheckboxGroup>
+    </div>
     <!-- 表格的数据是在全局的，但是列定义是在组件中的 -->
     <!-- 为什么数据在全局，但是列定义在组件中？ -->
     <!-- 原因是：全局数据是“数据”形式数据，组件中的数据是“UI”形式数据 -->
@@ -15,33 +25,47 @@
     created() {
       // 发出请求数据的命令，注意这里是异步的，所以是dispatch
       // 以前这里就axios.get()了，但是现在不了
-      this.$store.dispatch('carStore/loadData');
+      this.$store.dispatch("carStore/loadData");
     },
     data() {
       return {
         columns: [{
-            'title': 'id',
-            'key': 'id'
+            title: "id",
+            key: "id"
           },
           {
-            'title': '品牌',
-            'key': 'brand'
+            title: '图片',
+            key: 'img',
+            width: 160,
+            render: (h, {
+              row
+            }) => {
+              return h('img', {
+                attrs: {
+                  src: `http://aiqianduan.com:56506/images/carimages_small/${row.id}/view/${row.img}`
+                }
+              })
+            }
           },
           {
-            'title': '车系',
-            'key': 'series'
+            title: "品牌",
+            key: "brand"
           },
           {
-            'title': '颜色',
-            'key': 'color'
+            title: "车系",
+            key: "series"
           },
           {
-            'title': '排放',
-            'key': 'exhaust'
+            title: "颜色",
+            key: "color"
           },
           {
-            'title': '发动机',
-            'key': 'engine'
+            title: "排放",
+            key: "exhaust"
+          },
+          {
+            title: "发动机",
+            key: "engine"
           }
         ]
       };
@@ -49,7 +73,7 @@
     // 计算后，这样就可以把$store.state.carStore给变短
     computed: {
       carStore() {
-        return this.$store.state.carStore
+        return this.$store.state.carStore;
       }
     }
   };
--- 34_vuex汽车筛选器/src/store/carStore.js
+++ 34_vuex汽车筛选器/src/store/carStore.js
@@ -8,6 +8,8 @@ export default {
     total: 0,
     // 汽车数据
     cardata: [],
+    // 当前颜色筛选
+    color:['红','蓝']
   },
   // [同步的，被commit刺激的，唯一能够改变state的地方]
   mutations: {
@@ -23,23 +25,35 @@ export default {
     changePage(state, payloads) {
       state.page = payloads.page;
     },
+    // 更改颜色
+    changeColor(state,payloads){
+        state.color=payloads.color
+    }
   },
   // [异步的，被dispatch刺激的]
   actions: {
     // 拉取数据
     loadData({state, commit}) {
       axios
-        .get('http://www.aiqianduan.com:56506/cars?page=' + state.page)
-        .then((data) => {
+        .get('http://www.aiqianduan.com:56506/cars?page=' + state.page + '&color=' + state.color.join('v')).then((data) => {
           // 上跳
           commit('changeCardata', {cardata: data.data.results});
           commit('changeTotal', {total: data.data.total});
         });
     },
     // 改变页面
-    changePage({commit}, payloads) {
+    changePage({commit, dispatch}, payloads) {
       // 先要一次上跳,page就改变了
       commit('changePage', payloads);
+      // 然后拉取就行了
+      dispatch('loadData');
     },
+    // 改变颜色
+    changeColor({commit,dispatch},payloads){
+        // 改变颜色，页码要归1
+        commit('changePage',{page:1});
+        commit('changeColor',payloads);
+        dispatch('loadData')
+    }
   },
 };

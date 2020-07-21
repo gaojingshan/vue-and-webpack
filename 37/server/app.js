d8fff959e6008163b7700ca1825a50755a510b9e
高境杉 <1359287622@qq.com>
2020-07-21 21:54:36 +0800

    server

--- 37/server/app.js
+++ 37/server/app.js
@@ -13,7 +13,7 @@ app.get('/tags', (req, res) => {
 });
 
 // 删除
-app.get('/tags/del/:indexArr', (req, res) => {
+app.delete('/tags/del/:indexArr', (req, res) => {
   var indexArr = req.params.indexArr.split('v');
   // 把这里面每项都变为数字，因为这样才能下面和includes比对
   indexArr = indexArr.map((item) => Number(item));
@@ -32,7 +32,7 @@ app.get('/tags/del/:indexArr', (req, res) => {
 });
 
 // 上移一项
-app.get('/tags/prev/:index', (req, res) => {
+app.patch('/tags/prev/:index', (req, res) => {
   const index = Number(req.params.index);
   if (index <= 0) {
     res.send('nook');
@@ -47,6 +47,47 @@ app.get('/tags/prev/:index', (req, res) => {
     var delitem = arr.splice(index, 1)[0];
     // 插入到前一项
     arr.splice(index - 1, 0, delitem);
+    // 写回去
+    fs.writeFile('./mysql.txt', JSON.stringify(arr), function () {
+      res.send('ok');
+    });
+  });
+});
+
+// 下移一项
+app.patch('/tags/next/:index', (req, res) => {
+  const index = Number(req.params.index);
+
+  // 读取文件
+  fs.readFile('./mysql.txt', (err, content) => {
+    // 读取arr
+    var arr = JSON.parse(content.toString());
+    // 移动，就相当于把那项删除了，然后插入到它前面一项
+    // 要移动的这项要删除，删除之后得到数组（得到被删除的项），所以要加方括号0
+    var delitem = arr.splice(index, 1)[0];
+    // 插入到前一项
+    arr.splice(index + 1, 0, delitem);
+    // 写回去
+    fs.writeFile('./mysql.txt', JSON.stringify(arr), function () {
+      res.send('ok');
+    });
+  });
+});
+
+// 目录，两个斜杠的往下写，三个斜杠的往上写，因为两个斜杠的:index会匹配任一项
+// 修改某个tag
+app.patch('/tags/:index', (req, res) => {
+  const index = Number(req.params.index);
+
+  // 读取文件
+  fs.readFile('./mysql.txt', (err, content) => {
+    // 读取arr
+    var arr = JSON.parse(content.toString());
+    // 移动，就相当于把那项删除了，然后插入到它前面一项
+    // 要移动的这项要删除，删除之后得到数组（得到被删除的项），所以要加方括号0
+    var delitem = arr.splice(index, 1)[0];
+    // 插入到前一项
+    arr.splice(index + 1, 0, delitem);
     // 写回去
     fs.writeFile('./mysql.txt', JSON.stringify(arr), function () {
       res.send('ok');

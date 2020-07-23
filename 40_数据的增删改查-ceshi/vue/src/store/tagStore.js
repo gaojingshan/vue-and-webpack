41d0848a2dc171e0658c2e44510fbfd10e74872b
高境杉 <1359287622@qq.com>
2020-07-23 23:00:04 +0800

    数据的增删改查

--- 40_数据的增删改查-ceshi/vue/src/store/tagStore.js
+++ 40_数据的增删改查-ceshi/vue/src/store/tagStore.js
@@ -10,10 +10,36 @@ export default {
     },
   },
   actions: {
+    // 列出
     loadTags({commit}) {
       axios.get('/api/tags').then((data) => {
         commit('changeTags', {tagslist: data.data});
       });
     },
+    // 上移
+    prev({commit, dispatch}, payloads) {
+      axios.patch('/api/tags/prev/' + payloads.index).then((data) => {
+        dispatch('loadTags');
+      });
+    },
+    // 下移
+    next({commit, dispatch}, payloads) {
+      axios.patch('/api/tags/next/' + payloads.index).then((data) => {
+        dispatch('loadTags');
+      });
+    },
+    // 删除
+    del({commit, dispatch}, payloads) {
+      axios.delete('/api/tags/' + payloads.index).then((data) => {
+        dispatch('loadTags');
+      });
+    },
+    // 批量删除
+    delete({commit, dispatch}, payloads) {
+      axios.delete('/api/tags/' + payloads.index.join('v')).then((data) => {
+        dispatch('loadTags');
+      });
+    },
+    // 
   },
 };

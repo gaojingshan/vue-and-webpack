24f012200f8bcfd09b76bef4c7c8091cf676e76f
高境杉 <1359287622@qq.com>
2020-08-04 21:17:25 +0800

    computed可以拆为get和set的情况1：props接收的参数，你要双向绑定到这个值

---
+++ 51_computed/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{a}}</h1>
+    <h1>{{b}}</h1>
+
+    <Haha :a="a" @changeA="a=$event" />
+    
+  </div>
+</template>
+
+<script>
+import Haha from "./components/Haha.vue";
+export default {
+  data() {
+    return {
+      a: 11
+    };
+  },
+  computed: {
+    b() {
+      return this.a * 2;
+    }
+  },
+  components: {
+    Haha
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ 51_computed/src/components/Haha.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="range" v-model="mya" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["a"],
+  computed: {
+    mya: {
+      get() {
+        return this.a;
+      },
+      set(v) {
+        this.$emit("changeA", v);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

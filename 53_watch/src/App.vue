b940c972f8032f49e0445c4cfb5c41f37b79cc58
高境杉 <1359287622@qq.com>
2020-08-04 21:56:55 +0800

    watch

---
+++ 53_watch/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Haha :a="a" @changeA="a=$event" />
+    </div>
+</template>
+
+<script>
+    import Haha from "./components/Haha.vue";
+    export default {
+        components: {
+            Haha
+        },
+        data() {
+            return {
+                a: 10
+            };
+        },
+        watch:{
+            a(v){
+                console.log(v);
+                
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 53_watch/src/components/Haha.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{mya}}
+        <input type="range" v-model="mya" />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["a"],
+        data() {
+            return {
+                mya: this.a
+            };
+        },
+        watch: {
+            mya(a) {
+                this.$emit("changeA", a);
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>

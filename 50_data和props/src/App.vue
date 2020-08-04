0af1ccf4d4d2ec7e0f90ba230b955039337c9ff3
高境杉 <1359287622@qq.com>
2020-08-04 21:06:39 +0800

    data和props

---
+++ 50_data和props/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{a}}
+    <Haha :a="a" />
+  </div>
+</template>
+
+<script>
+import Haha from "./components/Haha.vue";
+export default {
+  data() {
+    return {
+      a: 12
+    };
+  },
+  components: {
+    Haha
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ 50_data和props/src/components/Haha.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    a: {
+      type: Number,
+      default: 0,
+      required: true,
+      validator: function(value) {
+        return value >= 0;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

e4ef8e7ef1552f213a8facae3e4a7d1ab4aed470
高境杉 <1359287622@qq.com>
2020-08-05 21:00:50 +0800

    v-slot插槽有名字

---
+++ 61_内置指令 v-slot插槽有名字/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <Haha>
+        <template v-slot:toubu>
+          <h1>我是头部</h1>
+        </template>
+        <template v-slot:shenti>
+          <h1>我是身体</h1>
+        </template>
+        <template v-slot:weibu>
+          <h1>我是尾部</h1>
+        </template>
+      </Haha>
+    </div>
+</template>
+
+<script>
+import Haha from './components/Haha.vue';
+    export default {
+        components:{
+          Haha
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 61_内置指令 v-slot插槽有名字/src/components/Haha.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>我是哈哈组件</h1>
+        <slot name="toubu"></slot>
+        <slot name="shenti"></slot>
+        <slot name="weibu"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

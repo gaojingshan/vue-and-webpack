e57efe949cdcf02ef2d85744a81efce627c2ff8a
高境杉 <1359287622@qq.com>
2020-08-05 21:00:19 +0800

    v-model  v-pre

---
+++ 58_内置指令 v-model  v-pre/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{a}}
+    <!-- v-model 随表单控件类型不同而不同。 -->
+    <input type="text" v-model.lazy="a">
+    <input type="text" v-model.lazy.number.trim="a">
+    <!-- v-pre -->
+    <div v-pre>
+      {{a}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        a: 10
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

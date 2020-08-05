160fa21e0ed272333c5b4f7c2598c4e575fe4e31
高境杉 <1359287622@qq.com>
2020-08-05 21:01:03 +0800

    is作用1

---
+++ 62_特殊属性 is作用1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 作用① ： 使标签可以合理嵌套。 -->
+    <ul class="box">
+      <li is="Haha"></li>
+      <li is="Haha"></li>
+      <li is="Haha"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Haha from './components/Haha.vue';
+  export default {
+    components: {
+      Haha
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .box {
+    margin: 40px;
+  }
+</style>
---
+++ 62_特殊属性 is作用1/src/components/Haha.vue
@@ -0,0 +1,13 @@
+<template>
+    <li>
+        哈哈组件
+    </li>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="less" scoped>
+   
+</style>

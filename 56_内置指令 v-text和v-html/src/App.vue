3792a40ff8d4dc895e958d8cca3032861e869f7d
高境杉 <1359287622@qq.com>
2020-08-05 20:59:52 +0800

    v-text和v-html

---
+++ 56_内置指令 v-text和v-html/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 字符串或者变量，二者无差别。 -->
+    <div v-text="'哈哈'"></div>
+    <div v-html="'哈哈'"></div>
+
+    <div v-text="a"></div>
+    <div v-html="a"></div>
+
+    <!-- v-html注意：内容按普通 HTML 插入 - 不会作为 Vue 模板进行编译。 -->
+    <div v-text="'<i>哈哈</i>'">哈哈</div>
+    <div v-html="'<i>哈哈</i>'">哈哈</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        a: 10,
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>

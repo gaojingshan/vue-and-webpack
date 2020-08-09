177fcb17a5106d3642a01bfdce4f3f824bc0c02a
高境杉 <1359287622@qq.com>
2020-08-09 12:37:32 +0800

    Vue.component()注册全局组件

---
+++ 68_Vue.component()注册全局组件 直接写/src/index.js
@@ -0,0 +1,16 @@
+// import Vue from 'vue';
+import Vue from '../node_modules/vue/dist/vue.js';
+import App from './App.vue';
+
+// 注册全局组件，这样添加就无人不识君了。
+// 全局组件component准备开始
+// Vue.component后面可以写任何vue组件类的实例的写法
+Vue.component('Haha', {
+  template: '<div>我是哈哈哈</div>',
+});
+new Vue({
+  el: '#app',
+  render(h) {
+    return h(App);
+  },
+});
